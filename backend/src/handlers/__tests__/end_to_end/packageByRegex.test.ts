0f19c275d37fec4a36296d2fdac179bad2e96df7
Ryan Jordan <rtjord@gmail.com>
2024-11-26 16:58:26 -0600

    updated import statements for tests

--- backend/src/handlers/__tests__/end_to_end/packageByRegex.test.ts
+++ backend/src/handlers/__tests__/end_to_end/packageByRegex.test.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { PackageData, PackageRegEx, PackageMetadata } from "../../../future_lambda_layer/interfaces";
+import { PackageData, PackageRegEx, PackageMetadata } from "../../../common/interfaces";
 import { baseUrl } from "./config";
 
 
