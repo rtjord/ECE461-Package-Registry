abde935682e88c189ead7ae7202c3f87357ef316
Trent <seaman7@purdue.edu>
2024-11-26 14:01:57 -0500

    OK

--- backend/src/handlers/PackageCreate/index.ts
+++ backend/src/handlers/PackageCreate/index.ts
@@ -20,6 +20,14 @@ type User = typeof interfaces.User;
 type Package = typeof interfaces.Package;
 type PackageMetadata = typeof interfaces.PackageMetadata;
 
+
+const servicesPath = process.env.SERVICES_PATH || 'services/rate';
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const { npmAnalysis } = require(`${servicesPath}/tools/api`);
+
+
+
+
 import { createHash } from 'crypto';
 import JSZip from "jszip";
 import * as fs from 'fs';
@@ -91,7 +99,9 @@ export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayPr
         } else {
             return createErrorResponse(400, 'Invalid request. No valid content or URL provided.');
         }
+        const npmResults = await npmAnalysis(packageName);
 
+        console.log(npmResults);
         // Extract package.json and README.md from the zip file
         const { packageJson, readme } = await extractFilesFromZip(fileContent);
         // upload readme to opensearch in the future
