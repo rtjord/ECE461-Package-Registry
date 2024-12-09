b31da86f7aaafd826103f2adb1377b82eb3817bd
Ryan Jordan <rtjord@gmail.com>
2024-12-08 23:31:25 -0500

    added frontend test to ci cd

--- backend/src/handlers/__tests__/end_to_end/config.ts
+++ backend/src/handlers/__tests__/end_to_end/config.ts
@@ -1,2 +1,2 @@
-export const baseUrl = "https://dev.teamfivepackages.com";
-// export const baseUrl = "https://prod.teamfivepackages.com";
+// export const baseUrl = "https://dev.teamfivepackages.com";
+export const baseUrl = "https://prod.teamfivepackages.com";
